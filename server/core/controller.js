0730c34785a2ed0bb24f740bac7c0307269d2786
Akka Vodol <anatole.romon@polytechnique.edu>
2018-08-17 15:12:39 +0200

    Make core controller functions return promises if no callback is given

--- server/core/controller.js
+++ server/core/controller.js
@@ -2,79 +2,105 @@ class Controller {
     constructor (model) {
         this.model = model;
     }
+}
 
-    findAll (req, done) {
-        var Model = this.model;
-        var perPage = config.get('pagination.itemsPerPage');
-        var page = (req.query.page) ? req.query.page : 1;
-        var find = {};
-        var searchText = (req.query.search) ? req.query.search : false;
-        var filters = (req.query.filters) ? req.query.filters : false;
-        var fields = {};
-        var fieldsToGet = (req.query.fields) ? req.query.fields : false;
-        if (req.query.page > 0) { var params = (req.query.page) ? {skip: (page - 1) * perPage, limit: perPage} : {}; }
+Controller.prototype.findAll = asyncDecorator(findAll);
+Controller.prototype.findOne = asyncDecorator(findOne);
+Controller.prototype.findOneForServer = asyncDecorator(findOneForServer);
+Controller.prototype.create = asyncDecorator(create);
+Controller.prototype.update = asyncDecorator(update);
+Controller.prototype.remove = asyncDecorator(remove);
 
-        if (req.query.sort) {
-            var sortField = {};
+function asyncDecorator (f) {
+    /*
+    *   Widestage was initially built using only callbacks
+    *
+    *   This wrapper makes it possible to use promises for the core functions.
+    *   Core functions return promises if no done argument is passed
+    */
 
-            sortField[req.query.sort] = (req.query.sortType) ? req.query.sortType : 1;
-
-            params['sort'] = sortField;
+    return function (req, done) {
+        if (done) {
+            return f.bind(this)(req, done);
+        } else {
+            return new Promise((resolve, reject) => {
+                f.bind(this)(req, resolve);
+            });
         }
+    };
+}
 
-        if (fieldsToGet) {
-            if (typeof fieldsToGet === 'string') {
-                fields = JSON.parse(fieldsToGet);
-            } else {
-                for (const i in fieldsToGet) {
-                    fields[fieldsToGet[i]] = 1;
-                }
+function findAll (req, done) {
+    var Model = this.model;
+    var perPage = config.get('pagination.itemsPerPage');
+    var page = (req.query.page) ? req.query.page : 1;
+    var find = {};
+    var searchText = (req.query.search) ? req.query.search : false;
+    var fields = {};
+    var fieldsToGet = (req.query.fields) ? req.query.fields : false;
+    if (req.query.page > 0) { var params = (req.query.page) ? {skip: (page - 1) * perPage, limit: perPage} : {}; }
+
+    if (req.query.sort) {
+        var sortField = {};
+
+        sortField[req.query.sort] = (req.query.sortType) ? req.query.sortType : 1;
+
+        params['sort'] = sortField;
+    }
+
+    if (fieldsToGet) {
+        if (typeof fieldsToGet === 'string') {
+            fields = JSON.parse(fieldsToGet);
+        } else {
+            for (const i in fieldsToGet) {
+                fields[fieldsToGet[i]] = 1;
             }
         }
+    }
 
-        var mandatoryFilters = [];
+    var mandatoryFilters = [];
 
-        if (req.query.trash) {
-            var trashField = {};
-            trashField['nd_trash_deleted'] = false;
+    if (req.query.trash) {
+        var trashField = {};
+        trashField['nd_trash_deleted'] = false;
 
-            mandatoryFilters.push(trashField);
+        mandatoryFilters.push(trashField);
+    }
+
+    if (req.query.userid) {
+        var userField = {};
+        userField['user_id'] = req.user._id;
+
+        mandatoryFilters.push(userField);
+    }
+
+    if (req.query.companyid) {
+        var companyField = {};
+        companyField['companyID'] = req.user.companyID;
+
+        mandatoryFilters.push(companyField);
+    }
+
+    if (req.query.find) {
+        for (var i in req.query.find) { mandatoryFilters.push(req.query.find[i]); }
+    }
+
+    var searchFind = {};
+
+    if (searchText) {
+        var findFields = [];
+        var searchFields = this.searchFields;
+
+        for (const i in searchFields) {
+            var thisField = {};
+
+            // thisField[searchFields[i]] = {$regex : searchText};
+            thisField[searchFields[i]] = new RegExp(searchText, 'i'); // "i" is for case-insensitive
+
+            findFields.push(thisField);
         }
-
-        if (req.query.userid) {
-            var userField = {};
-            userField['user_id'] = req.user._id;
-
-            mandatoryFilters.push(userField);
-        }
-
-        if (req.query.companyid) {
-            var companyField = {};
-            companyField['companyID'] = req.user.companyID;
-
-            mandatoryFilters.push(companyField);
-        }
-
-        if (req.query.find) {
-            for (var i in req.query.find) { mandatoryFilters.push(req.query.find[i]); }
-        }
-
-        var searchFind = {};
-
-        if (searchText) {
-            var findFields = [];
-            var searchFields = this.searchFields;
-
-            for (const i in searchFields) {
-                var thisField = {};
-
-                // thisField[searchFields[i]] = {$regex : searchText};
-                thisField[searchFields[i]] = new RegExp(searchText, 'i'); // "i" is for case-insensitive
-
-                findFields.push(thisField);
-            }
-            searchFind = (findFields.length > 0) ? {$or: findFields} : {};
-        }
+        searchFind = (findFields.length > 0) ? {$or: findFields} : {};
+    }
 
         if (filters) {
             var filterObject = {};
@@ -86,90 +112,108 @@ class Controller {
 
         if (Object.keys(searchFind).length > 0) { mandatoryFilters.push(searchFind); }
 
-        if (mandatoryFilters.length > 0) { find = {$and: mandatoryFilters}; }
+    if (mandatoryFilters.length > 0) { find = {$and: mandatoryFilters}; }
 
-        Model.find(find, fields, params, function (err, items) {
-            if (err) {
-                done({result: 0, msg: 'A database error has occured : ' + String(err), error: err});
-            } else {
-                Model.count(find, function (err, count) {
-                    if (err) {
-                        done({result: 0, msg: 'A database error has occured : ' + String(err), error: err});
+    Model.find(find, fields, params, function (err, items) {
+        if (err) {
+            done({result: 0, msg: 'A database error has occured : ' + String(err), error: err});
+        } else {
+            Model.count(find, function (err, count) {
+                if (err) {
+                    done({result: 0, msg: 'A database error has occured : ' + String(err), error: err});
+                } else {
+                    if (req.query.page > 0) {
+                        done({result: 1, page: page, pages: ((req.query.page) ? Math.ceil(count / perPage) : 1), items: items});
                     } else {
-                        if (req.query.page > 0) {
-                            done({result: 1, page: page, pages: ((req.query.page) ? Math.ceil(count / perPage) : 1), items: items});
-                        } else {
-                            done({result: 1, page: 1, pages: 1, items: items});
-                        }
+                        done({result: 1, page: 1, pages: 1, items: items});
                     }
-                });
-            }
-        });
+                }
+            });
+        }
+    });
+}
+
+function findOne (req, done) {
+    if (!req.query.id) {
+        done({result: 0, msg: "'id' is required."});
+        return;
     }
 
-    findOne (req, done) {
-        if (!req.query.id) {
-            done({result: 0, msg: "'id' is required."});
-            return;
+    var find = generateFindFields(req, req.query.id);
+
+    this.model.findOne(find, {}, function (err, item) {
+        if (err) {
+            done({result: 0, msg: 'A database error has occured : ' + String(err), error: err});
+        } else if (!item) {
+            done({result: 0, msg: 'Item not found.'});
+        } else {
+            done({result: 1, item: item.toObject()});
         }
+    });
+}
 
-        var find = generateFindFields(req, req.query.id);
-
-        this.model.findOne(find, {}, function (err, item) {
-            if (err) {
-                done({result: 0, msg: 'A database error has occured : ' + String(err), error: err});
-            } else if (!item) {
-                done({result: 0, msg: 'Item not found.'});
-            } else {
-                done({result: 1, item: item.toObject()});
-            }
-        });
+function findOneForServer (req, done) {
+    if (!req.query.id) {
+        done({result: 0, msg: "'id' is required."});
+        return;
     }
 
-    findOneForServer (req, done) {
-        if (!req.query.id) {
-            done({result: 0, msg: "'id' is required."});
-            return;
+    var find = generateFindFields(req, req.query.id);
+
+    this.model.findOne(find, {}, function (err, item) {
+        if (err) {
+            done({result: 0, msg: 'A database error has occured : ' + String(err), error: err});
+        } else if (!item) {
+            done({result: 0, msg: 'Item not found.'});
+        } else {
+            done({result: 1, item: item});
         }
+    });
+}
 
-        var find = generateFindFields(req, req.query.id);
+function create (req, done) {
+    var data = req.body;
 
-        this.model.findOne(find, {}, function (err, item) {
-            if (err) {
-                done({result: 0, msg: 'A database error has occured : ' + String(err), error: err});
-            } else if (!item) {
-                done({result: 0, msg: 'Item not found.'});
-            } else {
-                done({result: 1, item: item});
-            }
-        });
+    if (req.query.userid) {
+        data.createdBy = (req.isAuthenticated()) ? req.user._id : null;
+        data.createdOn = new Date();
     }
 
-    create (req, done) {
-        var data = req.body;
+    if (req.query.companyid) {
+        data.companyID = (req.isAuthenticated()) ? req.user.companyID : null;
+    }
 
-        if (req.query.userid) {
-            data.createdBy = (req.isAuthenticated()) ? req.user._id : null;
-            data.createdOn = new Date();
+    if (req.query.trash) {
+        data.nd_trash_deleted = false;
+    }
+
+    var user = (req.isAuthenticated()) ? req.user.username : 'unsigned user';
+    if (!data.nd_history) data.nd_history = [];
+
+    data.nd_history.push({text: 'Created on ' + new Date() + ' by ' + user,
+        user_id: (req.isAuthenticated()) ? req.user._id : null,
+        user_name: (req.isAuthenticated()) ? req.user.username : null,
+        user_companyID: (req.isAuthenticated()) ? req.user.companyID : null,
+        user_companyName: (req.isAuthenticated()) ? req.user.companyName : null
+    });
+
+    this.model.create(data, function (err, item) {
+        if (err) {
+            done({result: 0, msg: 'A database error has occured : ' + String(err), error: err});
+        } else {
+            done({result: 1, msg: 'Item created', item: item.toObject()});
         }
+    });
+}
 
-        if (req.query.companyid) {
-            data.companyID = (req.isAuthenticated()) ? req.user.companyID : null;
-        }
+function update (req, done) {
+    var data = req.body;
+    var id = data._id;
 
-        if (req.query.trash) {
-            data.nd_trash_deleted = false;
-        }
+    var find = generateFindFields(req, id);
 
-        var user = (req.isAuthenticated()) ? req.user.username : 'unsigned user';
-        if (!data.nd_history) data.nd_history = [];
-
-        data.nd_history.push({text: 'Created on ' + new Date() + ' by ' + user,
-            user_id: (req.isAuthenticated()) ? req.user._id : null,
-            user_name: (req.isAuthenticated()) ? req.user.username : null,
-            user_companyID: (req.isAuthenticated()) ? req.user.companyID : null,
-            user_companyName: (req.isAuthenticated()) ? req.user.companyName : null
-        });
+    delete (data.id);
+    delete (data._id);
 
         const Model = this.model;
 
@@ -207,69 +251,52 @@ class Controller {
         });
     }
 
-    update (req, done) {
-        var data = req.body;
-        var id = data._id;
+    var user = (req.isAuthenticated()) ? req.user.username : 'unsigned user';
+    if (!data.nd_history) data.nd_history = [];
 
-        var find = generateFindFields(req, id);
+    data.nd_history.push({text: 'Updated on ' + new Date() + ' by ' + user,
+        user_id: (req.isAuthenticated()) ? req.user._id : null,
+        user_name: (req.isAuthenticated()) ? req.user.username : null,
+        user_companyID: (req.isAuthenticated()) ? req.user.companyID : null,
+        user_companyName: (req.isAuthenticated()) ? req.user.companyName : null
+    });
 
-        delete (data.id);
-        delete (data._id);
+    this.model.update(find, { $set: data }, function (err, result) {
+        if (err) {
+            done({result: 0, msg: 'A database error has occured : ' + String(err), error: err});
+        } else {
+            var numAffected = (typeof result.n === 'undefined') ? result.nModified : result.n; // MongoDB 2.X return n, 3.X return nModified?
 
-        if (req.query.userid) {
-            data.user_id = (req.isAuthenticated()) ? req.user._id : null;
-        }
-        if (req.query.companyid) {
-            data.companyID = (req.isAuthenticated()) ? req.user.companyID : null;
-        }
-
-        var user = (req.isAuthenticated()) ? req.user.username : 'unsigned user';
-        if (!data.nd_history) data.nd_history = [];
-
-        data.nd_history.push({text: 'Updated on ' + new Date() + ' by ' + user,
-            user_id: (req.isAuthenticated()) ? req.user._id : null,
-            user_name: (req.isAuthenticated()) ? req.user.username : null,
-            user_companyID: (req.isAuthenticated()) ? req.user.companyID : null,
-            user_companyName: (req.isAuthenticated()) ? req.user.companyName : null
-        });
-
-        this.model.update(find, { $set: data }, function (err, result) {
-            if (err) {
-                done({result: 0, msg: 'A database error has occured : ' + String(err), error: err});
+            if (numAffected > 0) {
+                done({result: 1, msg: numAffected + ' record updated.'});
             } else {
-                var numAffected = (typeof result.n === 'undefined') ? result.nModified : result.n; // MongoDB 2.X return n, 3.X return nModified?
-
-                if (numAffected > 0) {
-                    done({result: 1, msg: numAffected + ' record updated.'});
-                } else {
-                    done({result: 0, msg: 'Error updating record, no record have been updated'});
-                }
+                done({result: 0, msg: 'Error updating record, no record have been updated'});
             }
-        });
+        }
+    });
+}
+
+function remove (req, done) {
+    if (!req.params.id) {
+        done({result: 0, msg: "'id' is required."});
+        return;
     }
 
-    remove (req, done) {
-        if (!req.params.id) {
-            done({result: 0, msg: "'id' is required."});
-            return;
-        }
+    var find = generateFindFields(req, req.params.id);
+    this.model.remove(find, function (err, result) {
+        if (err) {
+            done({result: 0, msg: 'A database error has occured : ' + String(err), error: err});
+        } else {
+            result = result.result;
+            var numAffected = (typeof result.n === 'undefined') ? result.nModified : result.n; // MongoDB 2.X return n, 3.X return nModified?
 
-        var find = generateFindFields(req, req.params.id);
-        this.model.remove(find, function (err, result) {
-            if (err) {
-                done({result: 0, msg: 'A database error has occured : ' + String(err), error: err});
+            if (numAffected > 0) {
+                done({result: 1, msg: numAffected + ' items deleted.'});
             } else {
-                result = result.result;
-                var numAffected = (typeof result.n === 'undefined') ? result.nModified : result.n; // MongoDB 2.X return n, 3.X return nModified?
-
-                if (numAffected > 0) {
-                    done({result: 1, msg: numAffected + ' items deleted.'});
-                } else {
-                    done({result: 0, msg: 'Error deleting items, no item have been deleted'});
-                }
+                done({result: 0, msg: 'Error deleting items, no item have been deleted'});
             }
-        });
-    }
+        }
+    });
 }
 
 function generateFindFields (req, id) {
